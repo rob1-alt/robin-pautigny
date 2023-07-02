451823e39560d0e3334544b4d69f768ea80601e6
Robin <robin.pautigny@hetic.net>
2023-07-02 11:08:44 +0000

    test install lenis

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Lenis as ReactLenis, useLenis } from '@studio-freight/react-lenis'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <ReactLenis root options={{}}>
         <Main />
         <NextScript />
+      </ReactLenis>
       </body>
     </Html>
   )
