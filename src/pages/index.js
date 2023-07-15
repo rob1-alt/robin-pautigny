0fe6ae81d7a9fc34c3358d6a170418554e1fc651
Robin <robin.pautigny@hetic.net>
2023-07-16 00:42:18 +0200

    adding metrics

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,11 +37,11 @@ export default function Home() {
         <meta property="og:image:alt" content="Thumbnail"/>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      {/* <script defer src="https://digianalytics.fr/pixel/PKR0TKFQlrW9n0lS"></script> */}
+      <script defer src="https://digianalytics.fr/pixel/PKR0TKFQlrW9n0lS"></script>
       <main className={styles.main}>
       <div className={styles.marquee1}>
   <div className={styles.track1}>
-    <div className={styles.content}>&nbsp;looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023 - looking for a work-linked placement as a Product Owner from 4 December 2023</div>
+    <div className={styles.content}>&nbsp;looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023</div>
   </div>
 </div>
         <CustomCursor/>
