3e20bf090b24b8476d99f0b26d130c9d1e4acbdf
Robin <robin.pautigny@hetic.net>
2023-07-17 07:42:50 +0200

    little title issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <script defer src="https://digianalytics.fr/pixel/PKR0TKFQlrW9n0lS"></script>
-      <main className={styles.main}>
+      <main className={styles.mainIndex}>
       <div className={styles.marquee1}>
   <div className={styles.track1}>
     <div className={styles.content}>&nbsp;looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023 - looking for a apprenticeship as a Product Owner from 4 December 2023</div>
