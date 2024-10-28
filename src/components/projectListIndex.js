5794167960a79affbefa8a25fa392b5244c048e9
rob1-alt <robin.pautigny@hetic.net>
2024-10-28 22:38:07 +0100

    add 4L trophy

--- src/components/projectListIndex.js
+++ src/components/projectListIndex.js
@@ -4,7 +4,8 @@ import styles from '@/styles/contextTual.module.css';
 import Link from "next/link";
 
 export default function ProjectsListIndex({ projects }) {
-  const [hoveredProject, setHoveredProject] = useState(null);
+  // Initialize with 0 to have first project hovered by default
+  const [hoveredProject, setHoveredProject] = useState(0);
   const videoRef = useRef(null);
 
   const playVideo = () => {
@@ -13,46 +14,47 @@ export default function ProjectsListIndex({ projects }) {
     }
   };
 
-  // const filteredProjects = projects.filter(project => !project.test);
-
-
   return (
-<div>
-
-    {/* <div className={styles.marquee}>
-      <p>PROJECTS • Projects </p>
-    </div> */}
-    <div className={styles.marquee}>
-  <div className={styles.track}>
-    <div className={styles.content}>&nbsp;PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS- PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS</div>
-  </div>
-</div>
-    <div className={styles.projectsList}>
-      {projects.map((project, index) => (
-        <div
-          key={index}
-          className={styles.project}
-          onMouseEnter={() => setHoveredProject(index)}
-          onMouseLeave={() => setHoveredProject(null)}
-        >
-          <Link href={`/projects/${project.slug}`}>
-            <div className={styles.listProjectTitle}>
-              <h2>{project.title}</h2>
-              <span>{project.content.year}</span>
-            </div>
-            <div>
-              {project.images.thumbnailVideo ? (
-                <video ref={videoRef} src={project.images.thumbnailVideo} alt={project.title} autoPlay muted playsInline onClick={playVideo} loop  />
-              ) : (
-                <img src={project.images.thumbnail} alt={project.title} />
-              )}
-          </div>
-          </Link>
+    <div>
+      <div className={styles.marquee}>
+        <div className={styles.track}>
+          <div className={styles.content}>&nbsp;PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS- PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS</div>
         </div>
-      ))}
-      
+      </div>
+      <div className={styles.projectsList}>
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className={styles.project}
+            onMouseEnter={() => {
+              // If hovering the first project, remove hover effect
+              if (index === 0) {
+                setHoveredProject(null);
+              } else {
+                setHoveredProject(index);
+              }
+            }}
+            onMouseLeave={() => {
+              // When mouse leaves, set hover back to first project
+              setHoveredProject(0);
+            }}
+          >
+            <Link href={`/projects/${project.slug}`}>
+              <div className={styles.listProjectTitle}>
+                <h2>{project.title}</h2>
+                <span>{project.content.year}</span>
+              </div>
+              <div>
+                {project.images.thumbnailVideo ? (
+                  <video ref={videoRef} src={project.images.thumbnailVideo} alt={project.title} autoPlay muted playsInline onClick={playVideo} loop  />
+                ) : (
+                  <img src={project.images.thumbnail} alt={project.title} />
+                )}
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
-    
-  </div>
   );
 }
